05b6fdaddb678072dd60115945a1b49feeb0c712
HomerNT <homer.nde-tsakou@polymtl.ca>
2022-04-19 22:21:22 -0400

    Update Sql-querries enum

--- server/app/services/constants/querries/sql-querries.ts
+++ server/app/services/constants/querries/sql-querries.ts
@@ -1,7 +1,7 @@
 export enum DatabaseQuery{
-    getAllTable = 'SELECT * FROM bdschema.$1;',
-    getGardenContent = "SELECT rangId, nomVariete, parcelleId FROM bdschema.ContenuRang WHERE jardinId = $1;",
-    getVarityDetails = "SELECT * FROM bdschema.Variete WHERE nom = $1",
+    getAllTable = 'SELECT * FROM bdschema.',
+    getGardenContent = "SELECT * FROM bdschema.ContenuRang WHERE jardinId = $1;",
+    getVarietyDetails = "SELECT * FROM bdschema.Variete WHERE nom = $1",
     insertPlant = "INSERT INTO bdschema.Plante(planteId, nomLatin, nomVariete, nom, categorie, type_, sousType) VALUES ($1, $2, $3, $4, $5, $6, $7);",
     deletePlant = "DELETE FROM bdschema.Plante WHERE planteId = $1;",
     searchPlant = "SELECT * FROM bdschema.Plante WHERE nom LIKE '%$1%';",
