648cdeba1478a0d89682fae1a64e434ea5eb66a5
HomerNT <homer.nde-tsakou@polymtl.ca>
2022-04-19 22:20:28 -0400

    Add garden interface

---
+++ server/app/interfaces/garden.ts
@@ -0,0 +1,10 @@
+export interface Garden {
+    gardenId: string;
+    name: string;
+    area: number;
+    isVegetableGarden: boolean;
+    typeSol: string;
+    isOrchardGarden: boolean;
+    maxHeight: number;
+    isOrnament: boolean;
+};
