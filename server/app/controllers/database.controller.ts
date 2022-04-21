7f14f5628c50bd3bd29d459c36b540b6c5d365c3
HomerNT <homer.nde-tsakou@polymtl.ca>
2022-04-20 23:35:20 -0400

    Add route that should not have been deleted

--- server/app/controllers/database.controller.ts
+++ server/app/controllers/database.controller.ts
@@ -63,6 +63,51 @@ export class DatabaseController {
       }
     });
     
+    // ====== GARDENS ROUTES =====
+    // GET path-to-server/database/gardens
+    router.get("/gardens", async (req: Request, res: Response, _: NextFunction) => {
+        try {
+          const gardensTable = await this.databaseService.getAllFromTable('Jardin'); // TODO: enum for tables table
+          res.json([...gardensTable.rows]);
+          
+        } catch (error) {
+            console.error(error.message);
+        }
+    });
+    router.get("/plants", async (req: Request, res: Response, _: NextFunction) => {
+      try {
+        const plantsTable = await this.databaseService.getAllFromTable('Plante');
+        res.json([...plantsTable.rows]);
+
+      } catch(error) {
+        console.error(error.message);
+      }
+
+    });
+    // https://stackoverflow.com/a/20386425
+    router.get("/plants/names/:name", async (req: Request, res: Response, _: NextFunction) => {
+      try {
+        // TODO: handle empty info
+        const name = req.params.name;
+        const gardenRowsContents = await this.databaseService.searchPlant(name);
+        res.json([...gardenRowsContents.rows]); // TODO this should not be an array
+        
+      } catch (error) {
+          console.error(error.message);
+      }
+    });
+    router.get("/gardens/:id", async (req: Request, res: Response, _: NextFunction) => {
+      try {
+        // TODO: handle empty info
+        const id = req.params.id;
+        const gardenRowsContents = await this.databaseService.getGardenContent(id);
+        res.json([...gardenRowsContents.rows]);
+        
+      } catch (error) {
+          console.error(error.message);
+      }
+  });
+
     return router;
   }
 }
