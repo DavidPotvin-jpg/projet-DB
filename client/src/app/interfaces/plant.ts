2bea6beb516aed07ba3d3e55a74c6153b3708a86
HomerNT <homer.nde-tsakou@polymtl.ca>
2022-04-21 00:03:09 -0400

    Add interface to the client

---
+++ client/src/app/interfaces/plant.ts
@@ -0,0 +1,9 @@
+export interface Plant {
+    planteId: string;
+    nomLatin: string;
+    nomVariete: string;
+    nom: string;
+    categorie: string;
+    type_: string;
+    sousType: string;
+}
