3096f1b1d7bb7922e465c1079d5420d29e758fb1
David <david.potvin@polymtl.ca>
2022-04-22 18:21:25 -0400

    Add uniqueness of variety verification

--- client/src/app/add-variety-page/add-variety-page.component.ts
+++ client/src/app/add-variety-page/add-variety-page.component.ts
@@ -13,6 +13,8 @@ export class AddVarietyPageComponent implements OnInit {
   public selectedSetupPeriod: number;
   public selectedHarvestPeriod: number;
   public varietyUploaded: boolean;
+  public serverVarieties: Variety[];
+  public errorMessage: string;
   constructor(private communicationService: CommunicationService) {
     this.variety = {
       nom: 'Legumieres',
@@ -32,15 +34,22 @@ export class AddVarietyPageComponent implements OnInit {
     this.selectedSetupPeriod = 1;
     this.selectedHarvestPeriod = 1;
     this.varietyUploaded = false;
+    this.serverVarieties = [];
+    this.errorMessage = '';
    }
 
   ngOnInit(): void {
   }
   addVariety(): void {
+    this.errorMessage = '';
     this.variety.periodemiseEnPlace = this.seasons[this.selectedSetupPeriod].name;
     this.variety.perioderecolte = this.seasons[this.selectedHarvestPeriod].name;
     if (!this.isFormFilled()) {
       this.varietyUploaded = false;
+      return;
+    }
+    if (!this.isVarietyUnique(this.variety.nom)) {
+      this.errorMessage = 'Le nom de cette variété existe déjà dans la base de donnnées';
       return;
     }
     this.communicationService.addVariety(this.variety).subscribe();
@@ -48,25 +57,35 @@ export class AddVarietyPageComponent implements OnInit {
     this.clearVariety();
   }
   isFormFilled(): boolean {
-    return (this.variety.nom.length > 0 &&
+    return (this.variety.nom.length > 1 && this.variety.nom.length <= 30 &&
     this.variety.anneedemiseenmarche > 0 &&
     this.variety.descriptionssemis.length > 0 &&
-     this.variety.plantation.length > 0 && this.variety.recolte.length > 0);
+     this.variety.plantation.length > 0 && this.variety.plantation.length <= 50 &&
+     this.variety.entretien.length > 0 && this.variety.entretien.length <= 50);
   }
   
   clearVariety() {
     this.selectedSetupPeriod = 1;
     this.selectedHarvestPeriod = 1;
     this.variety = {
-      nom: '',
+      nom: 'Legumieres',
       anneedemiseenmarche: 2022,
-      descriptionssemis: '',
-      plantation: '',
-      entretien: '',
-      recolte: '',
+      descriptionssemis: 'Terre souple',
+      plantation: 'Hautes herbes du Sud',
+      entretien: 'Une fois par jour',
+      recolte: 'Le soir',
       periodemiseEnPlace: '',
       perioderecolte: '',
       commentairegenerale: '',
     }
+  }
+
+  isVarietyUnique(varietyName: string) {
+    this.communicationService.getVarieties().subscribe( (result) => {
+      this.serverVarieties = result;
+    });
+    if (!this.serverVarieties) return false;
+    const result = this.serverVarieties.find(element => element.nom === varietyName);
+    return result === undefined;
   }
 }
